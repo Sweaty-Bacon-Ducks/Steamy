533ccbdc09e1938c89a869d27068222932f4c575
pindelm79 <m.pindel7991@gmail.com>
2017-11-22 18:20:40 +0100

    movement changes
    - mała zmiana logiki ruchu w x
    - upłynnienie ruchu

--- Steampunk/Assets/Scripts/PlayerController.cs
+++ Steampunk/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,9 @@ using UnityEngine;
 // dla mnie ładnie działało acc = 30, maxvel = 30, i howmuchfaster... = 2.
 // Polecam też zmienić w rigidbody linear drag na coś koło 1,5 coby się ładnie hamowało gdy nie ma inputu.
 
+// Zmiany:
+// - mała zmiana logiki ruchu w x
+// - dodane "granice przyspieszenia" w celu upłynnienia ruchu
 public class PlayerController : MonoBehaviour
 {
     /// <summary>
@@ -25,8 +28,17 @@ public class PlayerController : MonoBehaviour
     /// Ile razy szybciej obiekt zmienia kierunek niż przyspiesza.
     /// </summary>
     public float HowMuchFasterIsChangingDirection;
+    /// <summary>
+    /// Po jakiej części (w procentach) MaxVelocity przyspieszenie zostaje zmniejszone. (możliwe dodatkowe granice kiedyś)
+    /// </summary>
+    public float AccelerationCutThresholdPercent;
+    /// <summary>
+    /// Jaki procent przyspiesznia "zostaje" po przekroczeniu pewnej granicy (AccelerationCutThreshold) (w celu "upłynnienia" ruchu).
+    /// </summary>
+    public float AccelerationPercentAfterThreshold;
 
     private Rigidbody2D rb;
+
 
 	void Start()
     {
@@ -35,23 +47,33 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate()
     {
+        Move();
+	}
+
+    /// <summary>
+    /// Odpowiada za poruszanie obiektem.
+    /// </summary>
+    void Move()
+    {
         // Ustala wektor siły, jaka będzie dodana - zależna od inputu.
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector2 horizontalMovement = new Vector2(moveHorizontal, 0);
 
-        if (rb.velocity.magnitude <= MaxVelocity)
+        // Sprawdza (poprzez porównanie znaków inputu i aktualnej prędkości), czy gracz próbuje przyspieszyć, czy zmienić kierunek.
+        // Jeśli zmienia kierunek (tj, znaki są różne), działamy z większą siłą - chcemy aby zmiana była szybsza niż przyspieszanie.
+        if ((rb.velocity.x >= 0 && horizontalMovement.x <= 0) || (rb.velocity.x <= 0 && horizontalMovement.x >= 0))
         {
-            // Sprawdza (poprzez porównanie znaków inputu i aktualnej prędkości), czy gracz próbuje przyspieszyć, czy zmienić kierunek.
-            // Jeśli przyspiesza, dodajemy standardową siłę.
-            if ((rb.velocity.x >= 0 && horizontalMovement.x >= 0) || (rb.velocity.x <= 0 && horizontalMovement.x <= 0))
-            {
-                rb.AddForce(horizontalMovement * Acceleration);
-            }
-            // W innym wypadku działamy większą siłą - chcemy, aby zmiana kierunku była szybsza.
-            else
-            {
-                rb.AddForce(horizontalMovement * Acceleration * HowMuchFasterIsChangingDirection);
-            }
+            rb.AddForce(horizontalMovement * Acceleration * HowMuchFasterIsChangingDirection);
         }
-	}
+        // W innym wypadku przyspieszamy normalnie - ale tylko jeśli aktualna prędkość nie przekracza odpowiedniej części max.
+        else if (rb.velocity.magnitude <= MaxVelocity * (AccelerationCutThresholdPercent / 100))
+        {
+            rb.AddForce(horizontalMovement * Acceleration);
+        }
+        // Jeśli przekracza tę część - przypieszamy mniej gwałtownie.
+        else if (rb.velocity.magnitude <= MaxVelocity)
+        {
+            rb.AddForce(horizontalMovement * Acceleration * (AccelerationPercentAfterThreshold / 100));
+        }
+    }
 }
