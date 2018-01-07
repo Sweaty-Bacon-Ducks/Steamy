9c0c27ba557a8a1f5934449cb51d9df74ae04dbd
KowalikJakub <kowalik.kub@gmail.com>
2018-01-07 23:42:05 +0100

    Initiall files

--- 2DPlatformer/Assets/_Scripts/Player Code/PlayerController.cs
+++ 2DPlatformer/Assets/_Scripts/Player Code/PlayerController.cs
@@ -1,16 +1,21 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
-{ 
-#region Weapon
+{
+    #region Weapon
     public Weapon CurrentWeapon;
-#endregion
+    #endregion
 
-#region Fields
+    #region Fields
     [HideInInspector]
     public Rigidbody2D Body;
+
+    [Header("Player statistics")]
+    [SerializeField]
+    private new string name = "Default";
+    [SerializeField]
+    private float hp;
+    [SerializeField]
+    private float maxHP = 1;
 
     [Header("Controls")]
     public KeyCode ShootKey;
@@ -26,44 +31,96 @@ public class PlayerController : MonoBehaviour
     public bool IsStanding;  //w sensie stoi i nic nie robi
     public bool IsCrouching;
     public bool IsLying;     //postać leży
-#endregion
+
+    #endregion
+
+    #region Properties
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    public float HP
+    {
+        get
+        {
+            return hp;
+        }
+    }
+
+    public float MaxHP
+    {
+        get
+        {
+            return maxHP;
+        }
+    }
+    #endregion
+
+    void Start()
+    {
+        maxHP = hp;
+    }
+
     void Awake()
     {
         Body = GetComponent<Rigidbody2D>();
     }
+
     void FixedUpdate()
     {
-        if(Input.GetKey(ShootKey))
+        if (Input.GetKey(ShootKey))
         {
             CurrentWeapon.Shoot();
         }
-        if(Input.GetKey(ReloadKey))
+        if (Input.GetKey(ReloadKey))
         {
             CurrentWeapon.Reload();
         }
-        if(Input.GetKey(MoveKey))
+        if (Input.GetKey(MoveKey))
         {
             Move();
         }
-        if(Input.GetKey(SprintKey))
+        if (Input.GetKey(SprintKey))
         {
             Sprint();
         }
-        if(Input.GetKey(JumpKey))
+        if (Input.GetKey(JumpKey))
         {
             Jump();
         }
     }
     void Move()
     {
-    
+
     }
     void Sprint()
     {
-    
+
     }
     void Jump()
     {
-    
+
     }
-} 
+
+    public void Heal(float ammount)
+    {
+        hp += ammount;
+        if (hp >= maxHP)
+        {
+            hp = maxHP;
+        }
+    }
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            //Kill the player
+            Destroy(gameObject);
+        }
+    }
+}
