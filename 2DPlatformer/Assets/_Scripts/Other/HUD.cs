f09ad57b388961a613e2d89775b4932a3ac730cb
KowalikJakub <kowalik.kub@gmail.com>
2018-03-07 14:35:20 +0100

    Feature network (#12)
    * Initiall files
    
    * Some changes
    
    * Movement Sync
    
    * Rotation Sync

--- 2DPlatformer/Assets/_Scripts/Other/HUD.cs
+++ 2DPlatformer/Assets/_Scripts/Other/HUD.cs
@@ -1,60 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
 
-//[RequireComponent(typeof(Image))]
 public class HUD : MonoBehaviour
 {
-    float maxHp = 100;
-    [Range(0,100)]
-    public float hp = 50;
-    //public Image obrazek;
-    Image image;
-    Text ammoText;
+    private PlayerController player;
 
-    public int ammo = 0;
-    // Use this for initialization
+    public Image image;
+    public Text ammoText;
+
     void Start()
     {
-        //foreach (Transform child in transform)
-        //{
-        //    child.position += Vector3.up * 10.0F;
-        //}
-        //Transform hptrans = transform.GetChild(0);
-        Transform hptrans = transform.Find("HP");
-        image = hptrans.GetComponent<Image>();
-        //image = GetComponentInChildren<Image>();
-        image.type = Image.Type.Filled;
-        image.fillMethod = Image.FillMethod.Vertical;
-        image.fillOrigin = (int)Image.OriginVertical.Bottom;
-        Debug.Log((Image.OriginVertical)image.fillOrigin);
-        
-
-        Transform ammotrans = transform.Find("Ammo");
-
-        ammoText = ammotrans.GetComponent<Text>();
+        SetHUD();
     }
 
-    //public static List<GameObject> Initialize(Transform player)
-    //{
-    //    foreach (Transform tr in player)
-    //    {
-    //        if (tr != player.root && !AllWeapons.Contains(tr.gameObject))
-    //        {
-    //            AllWeapons.Add(tr.gameObject);
-    //        }
-    //    }
-    //    return AllWeapons;
-    //}
+    void Awake()
+    {
+        player = transform.root.GetComponent<PlayerController>();
+    }
 
-
-    // Update is called once per frame
     void Update()
     {
-        image.fillAmount = hp / maxHp;
+        image.fillAmount = player.HP / player.MaxHP;
+        ammoText.text = player.CurrentWeapon.CurrentAmmo.ToString();
+    }
 
-        ammoText.text = ammo.ToString();
+    void SetHUD()
+    {
+        image.fillAmount = 1;
+        //ammoText.text = player.CurrentWeapon.CurrentAmmo.ToString() + "/" + player.CurrentWeapon.MaxAmmo.ToString();
     }
 }
--- 2DPlatformer/Assets/_Scripts/Player Code/PlayerController.cs
+++ 2DPlatformer/Assets/_Scripts/Player Code/PlayerController.cs
@@ -1,69 +1,166 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
-{ 
-#region Weapon
+{
+    #region Weapon
     public Weapon CurrentWeapon;
-#endregion
+    #endregion
 
-#region Fields
+    #region Fields
     [HideInInspector]
     public Rigidbody2D Body;
+
+    [Header("Player statistics")]
+    [SerializeField]
+    private new string name = "Default";
+    [SerializeField]
+    private float hp;
+    [SerializeField]
+    private float maxHP = 1;
 
     [Header("Controls")]
     public KeyCode ShootKey;
     public KeyCode ReloadKey;
-    public KeyCode MoveKey;
+    //public KeyCode MoveKey;
     public KeyCode SprintKey;
     public KeyCode JumpKey;
 
     [Header("Player status")]
     public bool IsMoving;
-    public bool IsJumping;
+    public bool IsGrounded;  // czepiając się, można by zmienić IsGrounded na cos w stylu IsGrounded, bo bycie w powietrzu nie zawsze == skakanie
     public bool IsSprinting;
-    public bool IsStanding;  //w sensie stoi i nic nie robi
+    public bool IsStanding;  
     public bool IsCrouching;
-    public bool IsLying;     //postać leży
-#endregion
+    public bool IsLying;
+
+    [Header("Movement")]
+    public float Speed;
+    public float JumpForce;
+    #endregion
+
+    #region Properties
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    public float HP
+    {
+        get
+        {
+            return hp;
+        }
+    }
+
+    public float MaxHP
+    {
+        get
+        {
+            return maxHP;
+        }
+    }
+    #endregion
+
+    void Start()
+    {
+        maxHP = hp;
+    }
+
     void Awake()
     {
         Body = GetComponent<Rigidbody2D>();
     }
+
     void FixedUpdate()
     {
-        if(Input.GetKey(ShootKey))
+        Move();
+
+        if (Input.GetKey(ShootKey))
         {
             CurrentWeapon.Shoot();
         }
-        if(Input.GetKey(ReloadKey))
+        if (Input.GetKey(ReloadKey))
         {
             CurrentWeapon.Reload();
         }
-        if(Input.GetKey(MoveKey))
-        {
-            Move();
-        }
-        if(Input.GetKey(SprintKey))
-        {
-            Sprint();
-        }
-        if(Input.GetKey(JumpKey))
+        if (Input.GetKey(JumpKey))
         {
             Jump();
         }
+        if (Input.GetKey(SprintKey))
+        {
+            IsSprinting = true;
+            Sprint();
+        }
+        else
+        {
+            IsSprinting = false;
+        }
+        
     }
     void Move()
     {
-    
+        // Czy jest potrzebne zarówno IsMoving i IsStanding?
+        if (Body.velocity.x == 0 && Body.velocity.y == 0)
+        {
+            IsMoving = false;
+            IsStanding = true;
+        }
+        else
+        {
+            IsMoving = true;
+            IsStanding = false;
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        // Body.velocity, bo w ten sposób ruch jest dużo bardziej responsywny.
+        Body.velocity = new Vector2(moveHorizontal * Speed, Body.velocity.y);
     }
     void Sprint()
     {
-    
+        // Sprint działa tylko gdy jest wciśnięty jakiś klawisz ruchu i nie w powietrzu.
+        if (IsGrounded == false && Input.GetAxis("Horizontal") != 0)
+        {
+            Body.velocity = new Vector2(Body.velocity.x * 2, Body.velocity.y);
+        }
     }
     void Jump()
     {
-    
+        // Na razie bez double jump.
+        if (IsGrounded == false)
+        {
+            Vector2 jump = new Vector2(0, JumpForce);
+            Body.AddForce(jump, ForceMode2D.Impulse);
+        }
     }
-} 
+
+    public void Heal(float ammount)
+    {
+        hp += ammount;
+        if (hp >= maxHP)
+        {
+            hp = maxHP;
+        }
+    }
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            //Kill the player
+            Destroy(gameObject);
+        }
+    }
+    //Trzeba się zastanowić jak będziemy rozpoznawać poszczególne obiekty i które przez tagi a które przez warstwy
+    /*void OnCollisionEnter2D()
+    {
+        IsGrounded = false;
+    }
+    void OnCollisionExit2D()
+    {
+        IsGrounded = true;
+    }
+    */
+}
