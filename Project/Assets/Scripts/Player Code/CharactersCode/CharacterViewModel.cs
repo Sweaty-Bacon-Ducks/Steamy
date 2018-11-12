447a88a82c1955e9808aa65202fe9bad454354ea
Kowalik Jakub <jakub.kowalik@grupawp.pl>
2018-11-12 14:18:45 +0100

    Created new input system

---
+++ Project/Assets/Scripts/Player Code/CharactersCode/CharacterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Steamy.Player.MotionModes;
+
+namespace Steamy.Player
+{
+	public class CharacterViewModel : MonoBehaviour
+	{
+		public CharacterModel Model;
+        public CharacterParameters Parameters;
+
+        private void Awake()
+        {
+            Model.ViewModel = this;
+        }
+
+        private void ApplyMotion()
+        {
+            foreach (var mode in Parameters.MotionModesSet)
+            {
+                mode.ApplyMotion(this);
+            }
+        }
+
+        private void FixedUpdate()
+		{
+			ApplyMotion();
+		}
+	}
+}
---
+++ Project/Assets/Scripts/Utility/Xml/EXmlSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Steamy.Utility.Xml
+{
+   
+    public static class EXmlSerializer
+    {
+        public static void UnknownNodeCallback(this XmlSerializer serializer,
+                                                 object sender,
+                                                 XmlNodeEventArgs e)
+        {
+
+        }
+
+        public static void UnknownAttributeCallback(this XmlSerializer serializer,
+                                                object sender,
+                                                XmlAttributeEventArgs e)
+        {
+
+        }
+    }
+}
