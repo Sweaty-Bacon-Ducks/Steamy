5de5b19b3d457f6ddf64dfd4ef4480d78d4e0b0b
Marcin Krupa <marcin.krupa.96@gmail.com>
2018-11-17 19:00:04 +0100

    Added a basic animation controller.

---
+++ Project/Assets/Scripts/Player Code/CharactersCode/AnimationConfigurator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationConfigurator : MonoBehaviour
+{
+    Animator animator;
+    float speedPercent;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+
+    void Update()
+    {
+        speedPercent = animator.GetFloat("speedPercent");
+        if (speedPercent < 1)
+        {
+            animator.SetFloat("speedPercent", speedPercent + 0.005f, .1f, Time.deltaTime);
+        }
+    }
+}
