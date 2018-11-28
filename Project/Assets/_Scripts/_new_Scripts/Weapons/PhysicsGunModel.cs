517785cb049b8a2f2d6aee07cd63612a18aa8f95
Kacper Obara <kacper97pl@tlen.pl>
2018-11-28 18:20:37 +0100

    Added the base of WeaponSystem (#61)
    * initial commit
    
    * Poprawka

---
+++ Project/Assets/_Scripts/_new_Scripts/Weapons/PhysicsGunModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Weapons/PhysicsGun")]
+public class PhysicsGunModel : WeaponModel {
+
+    public float BulletSpread;
+    public int BulletCount;
+    public int MagazineSize;
+
+    public float BulletLifeTime;
+    public float BulletSpeed;
+
+    public int BulletsInMagazine;
+    public float ReloadTimer;
+    public float FireTimer;
+    public float TriggerTimer;
+}
