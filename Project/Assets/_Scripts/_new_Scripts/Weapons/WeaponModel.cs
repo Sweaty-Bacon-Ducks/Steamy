7796f8cb630c9510ab0edc257ec0cf3dc82a88a2
Kacper Obara <kacper97pl@tlen.pl>
2018-11-28 18:20:37 +0100

    Added the base of WeaponSystem (#61)
    * initial commit
    
    * Poprawka

---
+++ Project/Assets/_Scripts/_new_Scripts/Weapons/WeaponModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName ="Weapons/MeleeWeapon")]
+public class WeaponModel : ScriptableObject {
+
+    public string Name;
+    public string Desc;
+    public Sprite Sprite;
+    public float Damage;
+}
