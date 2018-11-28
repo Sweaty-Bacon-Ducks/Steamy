1a594b83287eb2edba6692e9e3e502a19a531b5a
Kacper Obara <kacper97pl@tlen.pl>
2018-11-28 15:35:01 +0100

    initial commit

---
+++ Project/Assets/_Scripts/_new_Scripts/Weapons/RaycastGunModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapons/RaycastGun")]
+public class RaycastGunModel : ScriptableObject {
+
+    public float BulletSpread;
+    public int BulletCount;
+    public int MagazineSize;
+
+    public float RaycastLength;
+
+    public int BulletsInMagazine;
+    public float ReloadTimer;
+    public float FireTimer;
+    public float TriggerTimer;
+}
