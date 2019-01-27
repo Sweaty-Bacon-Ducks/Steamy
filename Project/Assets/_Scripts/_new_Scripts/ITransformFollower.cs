d8f57ca22a1ed76d68ce6d29ae47940269d85d8d
KowalikJakub <kowalik.kub@gmail.com>
2019-01-27 18:32:53 +0100

    Feature/camera controllers (#76)
    * Added working camera when connected to game
    
    And moved character specific SO to character folder
    
    * Added EZ Camera Shake
    
    * changed camera position assingment to localPosition 
    * wrapped the Camera in a CameraHolder (empty gameobject)
    
    * Fix script and class name
    
    * Added TransformFollower and fixed NullRef with characterCamera
    
    * Unsaved prefab changes
    
    * Added default camera target

---
+++ Project/Assets/_Scripts/_new_Scripts/ITransformFollower.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface ITransformFollower
+{
+	Transform Target { get; set; }
+	void Follow();
+}
---
+++ Project/Assets/_Scripts/_new_Scripts/Player Code/Camera/CameraFollower.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+namespace Steamy
+{
+	[RequireComponent(typeof(Camera))]
+	public class CameraFollower : MonoBehaviour, ITransformFollower
+	{
+		private Camera m_Camera;
+
+		[SerializeField]
+		private Transform m_DefaultScenePosition;
+
+		[SerializeField]
+		private Transform m_Target;
+
+		public Transform DefaultScenePosition
+		{
+			get { return m_DefaultScenePosition; }
+			set { m_DefaultScenePosition = value; }
+		}
+
+		public Transform Target
+		{
+			get { return m_Target; }
+			set { m_Target = value; }
+		}
+
+		public Vector3 PositionOffset;
+		public float MovementSpeed = 8f;
+		public float MouseRange = 5f;
+
+		private void Start()
+		{
+			m_Camera = GetComponent<Camera>();
+		}
+
+		private void LateUpdate()
+		{
+			if (Target != null)
+				Follow();
+			else if (m_DefaultScenePosition)
+				Target = m_DefaultScenePosition;
+		}
+
+		public void Follow()
+		{
+			var targetPosition = Target.position;
+			var cursorPosition = m_Camera.ScreenToWorldPoint(new Vector3
+			{
+				x = Input.mousePosition.x,
+				y = Input.mousePosition.y,
+				z = MouseRange
+			});
+
+			var targetCameraPosition = GetTargetCameraPosition(cursorPosition, targetPosition);
+			transform.localPosition = Vector3.Lerp(transform.position, targetCameraPosition, Time.deltaTime * MovementSpeed);
+		}
+
+		private Vector3 GetTargetCameraPosition(Vector3 start, Vector3 end)
+		{
+			return new Vector3
+			{
+				x = (start.x + end.x) / 2 + PositionOffset.x,
+				y = (start.y + end.y) / 2 + PositionOffset.y,
+				z = end.z + PositionOffset.z
+			};
+		}
+	}
+}
+
