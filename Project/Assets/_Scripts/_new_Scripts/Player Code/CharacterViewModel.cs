6773a3d2c1b1db53e1a4c67459efc6a9d06424a8
Jakub <kowalik.kub@gmail.com>
2019-01-27 20:10:18 +0100

    Added health synchronization

--- Project/Assets/_Scripts/_new_Scripts/Player Code/CharacterViewModel.cs
+++ Project/Assets/_Scripts/_new_Scripts/Player Code/CharacterViewModel.cs
@@ -1,8 +1,7 @@
 ﻿using System.ComponentModel;
-
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 namespace Steamy.Player
 {
@@ -74,6 +73,8 @@ namespace Steamy.Player
 		#endregion
 
 		#region PrivateInterface
+		[SyncVar] private double m_CurrentHealth;
+
 		private bool IsCharacterDead
 		{
 			get
@@ -93,6 +94,7 @@ namespace Steamy.Player
 		}
 		private void OnHealthChanged(object sender, PropertyChangedEventArgs e)
 		{
+			m_CurrentHealth = Model.Health.Value;
 			if (HealthView)
 			{
 				HealthView.text = Model.Health.Value.ToString();
