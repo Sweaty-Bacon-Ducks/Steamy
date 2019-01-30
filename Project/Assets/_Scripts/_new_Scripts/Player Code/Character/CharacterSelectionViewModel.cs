bc1cb5e49ac5e64e510ed79535d4b89381360e29
Jakub <kowalik.kub@gmail.com>
2019-01-30 23:24:21 +0100

    Created sample scene with basic UI controls
    Added Textmeshpro

---
+++ Project/Assets/_Scripts/_new_Scripts/Player Code/Character/CharacterSelectionViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectionViewModel : MonoBehaviour
+{
+	[SerializeField]
+	private List<GameObject> availableCharacters;
+
+	public string SceneCameraTag;
+	private GameObject sceneCamera;
+	public List<GameObject> AvailableCharacters
+	{
+		get { return availableCharacters; }
+		set { availableCharacters = value; }
+	}
+
+	// Use this for initialization
+	private void Start()
+	{
+		GameObject.FindGameObjectWithTag(SceneCameraTag).SetActive(false);
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+
+	}
+
+	private void OnDisable()
+	{
+		GameObject.FindGameObjectWithTag(SceneCameraTag).SetActive(true);
+	}
+}
