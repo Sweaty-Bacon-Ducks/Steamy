e2873d67c6df9cb07e8c45ffe11ae1fe9262f1ea
Jakub <kowalik.kub@gmail.com>
2019-02-03 19:47:42 +0100

    Added fonts scrollbar and listing elements

---
+++ Project/Assets/_Scripts/_new_Scripts/UI/CharacterListingElement.cs
@@ -0,0 +1,25 @@
+﻿using Steamy.Player;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Steamy.UI
+{
+	public class CharacterListingElement : MonoBehaviour
+	{
+		private TextMeshProUGUI textField;
+		private Image imageField; 
+
+		private void Awake()
+		{
+			textField = GetComponentInChildren<TextMeshProUGUI>();
+			imageField = GetComponentInChildren<Image>();
+		}
+
+		public void PresentCharacterData(CharacterModel characterModel)
+		{
+			textField.text = characterModel.Name;
+		}
+	}
+}
+
---
+++ Project/Assets/_Scripts/_new_Scripts/UI/ScrollbarView/MouseWheelScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseWheelScroll : MonoBehaviour
+{
+	[Range(min: -1, max: 1, order = 0)]
+	public float Smoothing;
+
+	private Scrollbar targetScrollbar;
+
+	private void Awake()
+	{
+		targetScrollbar = GetComponent<Scrollbar>();
+	}
+
+	private void Update()
+	{
+		var mouseWheel = Input.mouseScrollDelta;
+		targetScrollbar.value += mouseWheel.y * Smoothing;
+	}
+}
