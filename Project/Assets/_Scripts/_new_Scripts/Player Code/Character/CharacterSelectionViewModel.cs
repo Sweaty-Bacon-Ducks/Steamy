1277c5b364f4dafe253e250788da5d68240cd43e
Jakub <kowalik.kub@gmail.com>
2019-02-10 12:26:01 +0100

    Created sample scene with basic UI controls
    Added Textmeshpro
    Added fonts scrollbar and listing elements
    
    
    Added Presenters and ViewModel 
    
    
    Started to work on player view model
    
    
    Merge remote-tracking branch 'origin/feature/CharacterSelection' into feature/CharacterSelection

---
+++ Project/Assets/_Scripts/_new_Scripts/Player Code/Character/CharacterSelectionViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using Steamy.Player;
+
+namespace Steamy.UI
+{
+    public class CharacterSelectionViewModel : MonoBehaviour
+    {
+        [SerializeField]
+        private List<GameObject> availableCharacters;
+        public List<GameObject> AvailableCharacters
+        {
+            get => availableCharacters;
+            set => availableCharacters = value;
+        }
+
+		public Transform ListingContent;
+		public GameObject ListingElementPrefab;
+
+        public string SceneCameraTag;
+        private GameObject sceneCamera;
+
+        private void Start()
+        {
+            sceneCamera = GameObject.FindGameObjectWithTag(SceneCameraTag);
+
+			foreach (var charater in AvailableCharacters)
+			{
+				var listingElement = Instantiate(ListingElementPrefab, ListingContent).
+					GetComponent<CharacterListingElement>();
+
+				var characterViewModel = charater.GetComponent<CharacterViewModel>();
+				listingElement.PresentCharacterData(characterViewModel.Model);
+			}
+
+        }
+    }
+}
+
