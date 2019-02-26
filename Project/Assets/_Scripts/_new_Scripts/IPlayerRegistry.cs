4b97a3d89ee6ac87714615e2b7310f939e618618
Jakub <kowalik.kub@gmail.com>
2019-02-26 22:49:05 +0100

    Added object registry

---
+++ Project/Assets/_Scripts/_new_Scripts/IPlayerRegistry.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface IObjectRegistry<T> where T : MonoBehaviour
+{
+	ICollection<T> Objects { get; }
+	T Find(string netId);
+	void Add(string netId, T player);
+	T Remove(string netId);
+}
