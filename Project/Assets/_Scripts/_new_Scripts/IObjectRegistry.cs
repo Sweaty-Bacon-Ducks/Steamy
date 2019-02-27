602d4f8fe0f2daf6c6b8609e7bce1ab4ba66f7cb
Jakub <kowalik.kub@gmail.com>
2019-02-27 18:20:17 +0100

    Deleted objects property

--- Project/Assets/_Scripts/_new_Scripts/IObjectRegistry.cs
+++ Project/Assets/_Scripts/_new_Scripts/IObjectRegistry.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public interface IObjectRegistry<T> where T : MonoBehaviour
 {
-	ICollection<T> Objects { get; }
 	T Find(string netId);
 	void Add(string netId, T player);
 	T Remove(string netId);
