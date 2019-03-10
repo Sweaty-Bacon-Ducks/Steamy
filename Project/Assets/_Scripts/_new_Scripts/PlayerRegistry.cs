9102ce6655ffd39fd35ec8da4ee5a9321e843dba
Jakub <kowalik.kub@gmail.com>
2019-03-10 11:54:36 +0100

    Added player registry

---
+++ Project/Assets/_Scripts/_new_Scripts/PlayerRegistry.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Steamy.Player
+{
+	public class PlayerRegistry : NetworkBehaviour, IObjectRegistry<PlayerViewModel>
+	{
+		private Dictionary<string, PlayerViewModel> playerRegistry;
+
+
+		public void Add(string netId, PlayerViewModel player)
+		{
+			playerRegistry[netId] = player;
+		}
+
+		public PlayerViewModel Find(string netId)
+		{
+			if (playerRegistry.ContainsKey(netId))
+			{
+				return playerRegistry[netId];
+			}
+			return null;
+		}
+
+		public PlayerViewModel Remove(string netId)
+		{
+			var player = Find(netId);
+			if (player)
+			{
+				playerRegistry.Remove(netId);
+			}
+			return player;
+		}
+
+	}
+}
