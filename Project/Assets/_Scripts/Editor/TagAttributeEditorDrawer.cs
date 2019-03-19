508b8835975c66e3e37b6e11c97fbb0d62c4b7e4
Jakub Kowalik <kowalik.kub@gmail.com>
2019-03-19 22:04:11 +0100

    Feature/player registry (#88)
    * Added health synchronization
    
    * Added health synchronization
    
    * Added health synchronization
    
    * Working weapons but without synchronization
    
    * Divided CharacterViewModel and Data Synchronization
    
    * Divided CharacterViewModel and Data Synchronization
    
    * Added player layer controller, fixed collision between players
    
    * Added tag property drawer
    
    * Added tag property drawer
    
    * Added tag property drawer
    
    
    Merge remote-tracking branch 'origin/feature/HealthSynchronization' into feature/HealthSynchronization
    
    * dunno 2
    
    * Added health synchronization
    
    * Working weapons but without synchronization
    
    * Divided CharacterViewModel and Data Synchronization
    
    
    Merge remote-tracking branch 'origin/feature/HealthSynchronization' into feature/HealthSynchronization
    Added player layer controller, fixed collision between players
    
    
    Added tag property drawer
    
    
    Merge remote-tracking branch 'origin/feature/HealthSynchronization' into feature/HealthSynchronization
    Merge remote-tracking branch 'origin/feature/HealthSynchronization' into feature/HealthSynchronization
    
    
    dunno 2
    
    * Added player registry
    
    * Added and tested player registry
    
    * Documentation fix

---
+++ Project/Assets/_Scripts/Editor/TagAttributeEditorDrawer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Steamy.Editor
+{
+    [CustomPropertyDrawer(typeof(TagAttribute))]
+    class TagAttributeEditorDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+        }
+    }
+}
---
+++ Project/Assets/_Scripts/_new_Scripts/Player Code/CharacterNetworkData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Steamy.Player
+{
+    [Serializable]
+    public struct CharacterNetworkData
+    {
+        public double Health;
+
+        public CharacterNetworkData(CharacterModel characterModel)
+        {
+            Health = characterModel.Health.Value;
+        }
+    }
+}
